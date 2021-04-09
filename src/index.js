cefa2326efa6e07699fd10c0f35c7569658ce2e9
Holovatiuk Victor <Stethem696@gmail.com>
2021-04-09 19:53:39 +0300

    в процессе выполнения ДЗ

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import './task-1.js';
 
 import './styles.css';
+// ----------------------------------------------------------------
+
+const promise = new Promise((resolve, reject) => {
+  const canFullFill = Math.random() > 0.5;
+
+  setTimeout(() => {
+    if (canFullFill) {
+      resolve('Все ок');
+    }
+    reject('ошибка');
+  }, 1000);
+});
+
+promise.then(onFullFilled, onRejected);
+
+function onFullFilled(result) {
+  console.log(result);
+}
+
+function onRejected(error) {
+  console.log(error);
+}
+
+// цепочки промисов
+
+promise
+  .then(onFullFilled)
+  .then(x => {
+    console.log(x);
+    return 10;
+  })
+  .then(y => {
+    console.log(y);
+  })
+  .catch(error => console.log(error));
